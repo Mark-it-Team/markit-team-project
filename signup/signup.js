af0826b39e0f69e5adc61a9554eaf8005a39daf9
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-28 11:27:01 -0800

    moved auth code from app.js to correct pages, made fetch vendors function

---
+++ signup/signup.js
@@ -0,0 +1,12 @@
+import { redirectIfLoggedIn, signupUser } from '../fetch-utils.js';
+
+signUpForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
