0d002f68a57200613d212a18a895ea0833692935
Kamie Short <kamie.short@gmail.com>
2022-03-01 11:26:01 -0800

    started display function

--- reserved/reserved.js
+++ reserved/reserved.js
@@ -1,7 +1,28 @@
-import { logout } from '../fetch-utils.js';
+import { logout, fetchCart } from '../fetch-utils.js';
+import { renderProduct } from '../render-utils.js';
 
 const logoutButton = document.getElementById('logout');
+const cartContainer = document.getElementById('cart-display');
+const productContainer = document.getElementById('products-container');
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
+
+export async function displayCart() {
+    const cart = await fetchCart();
+    cartContainer.append(renderProduct(cart));
+
+    const products = await fetchProducts();
+    for (let product of products) {
+        const productEl = renderProduct(product);
+        productContainer.append(productEl);
+
+        const i = await fetchItems();
+        for (let item of items) {
+            const itemEl = renderItems(item);
+            productContainer.append(productEl);
+        }
+    }
+}
+displayCart();
