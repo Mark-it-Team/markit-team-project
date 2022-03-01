297f9d6c9ee7faa92275d803dbf4b29109094597
Kamie Short <kamie.short@gmail.com>
2022-02-28 17:24:50 -0800

    product details displaying

--- vendor_detail/vendor_detail.js
+++ vendor_detail/vendor_detail.js
@@ -1,18 +1,31 @@
 import { logout, fetchProducts, fetchVendorDetails } from '../fetch-utils.js';
-import { renderVendorDetail } from '../render-utils.js';
+import { renderProduct, renderVendorDetail } from '../render-utils.js';
 
 const logoutButton = document.getElementById('logout');
 const params = new URLSearchParams(window.location.search);
 const vendorContainer = document.getElementById('vendor-container');
+const productContainer = document.getElementById('products-container');
+const homeBtn = document.getElementById('home');
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
+homeBtn.addEventListener('click', () => {
+    return (window.location.href = '../index.html');
+});
+
 export async function displayDetails() {
-    // const id = +params.get('id');
-    const vendor = await fetchVendorDetails(params.get('id'));
+    const data = +params.get('id');
+    const vendor = await fetchVendorDetails(data);
     vendorContainer.append(renderVendorDetail(vendor));
+
+    const products = await fetchProducts(data);
+    console.log(products);
+    for (let product of products) {
+        const productEl = renderProduct(product);
+        productContainer.append(productEl);
+    }
 }
 
 displayDetails();
