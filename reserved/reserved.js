5c3dacdbcc5b15b91be374e2081343e00c4844ab
Kamie Short <kamie.short@gmail.com>
2022-03-01 15:02:25 -0800

    home button

--- reserved/reserved.js
+++ reserved/reserved.js
@@ -3,16 +3,20 @@ import { renderProduct, renderCartItem } from '../render-utils.js';
 
 const logoutButton = document.getElementById('logout');
 const cartContainer = document.getElementById('cart-display');
-const productContainer = document.getElementById('products-container');
+const homeBtn = document.getElementById('home');
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
+homeBtn.addEventListener('click', () => {
+    return (window.location.href = '../index.html');
+});
+
 const vendors = await fetchVendors();
 
 for (let i = 1; i < vendors.length + 1; i++) {
-    const vendorProducts = (await fetchCart(i));
+    const vendorProducts = await fetchCart(i);
     for (let product of vendorProducts) {
         const item = product.products;
         if (item) {
