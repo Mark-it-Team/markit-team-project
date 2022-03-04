36a489e09030475934af23e9fd0460d609dcb071
m-ghiringhelli <mghiring@gmail.com>
2022-03-04 15:09:39 -0800

    fixed failing tests and deleted some comments

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,21 +1,29 @@
 import { renderVendor, renderVendorDetail, renderProduct } from '../render-utils.js';
+
 const test = QUnit.test;
 
 const testVendor = {
-    name: 'onlyPlants',
-    image: 'only_plants.jpg',
-    description: 'Your one-stop shop for sexy plants.'
+    id: 1,
+    created_at: '2022-02-28T18:49:26+00:00',
+    name: 'OnlyPlants',
+    description: "Portland's home for sexy plants since 2008. Come down and browse our selection of calatheas, dracaenas, and pothos. When you need a special plant, there's one place to go: OnlyPlants.",
+    image: 'only_plants.jpg'
 };
 
 const testProduct = {
-    name: 'plant1',
-    image: 'plant1.jpg',
+    id: 1,
+    created_at: '2022-02-28T23:34:58+00:00',
+    vendor_id: 1,
+    name: 'Calathea',
+    description: 'Calatheas are known for their beautiful foliage and tempermental personalities.',
+    image: '../assets/plant1.jpg',
+    price: '$200000000000000.00'
 };
 
 test('render vendor div should return a div for vendor', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="vendor"><img src="./assets/only_plants.jpg"><p>onlyPlants</p></div>`;
+    const expected = `<figure class="vendor-effect"><img src="./assets/only_plants.jpg"><figcaption><h2>OnlyPlants</h2><p>Portland's home for sexy plants since 2008. Come down and browse our selection of calatheas, dracaenas, and pothos. When you need a special plant, there's one place to go: OnlyPlants.</p></figcaption></figure>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -29,7 +37,7 @@ test('render vendor div should return a div for vendor', (expect) => {
 test('render vendor div should return a div for vendor detail', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="detail-container"><h2>onlyPlants</h2><img src="../assets/only_plants.jpg"><p>Your one-stop shop for sexy plants.</p></div>`;
+    const expected = `<div class="detail-container"><h2>OnlyPlants</h2><img src="../assets/only_plants.jpg"><p>Portland's home for sexy plants since 2008. Come down and browse our selection of calatheas, dracaenas, and pothos. When you need a special plant, there's one place to go: OnlyPlants.</p></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -43,7 +51,7 @@ test('render vendor div should return a div for vendor detail', (expect) => {
 test('render product should return a div for product', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="product"><img src="../assets/plant1.jpg"><p>plant1</p></div>`;
+    const expected = `<div class="product"><a href="#" class="column" id="caption"><span class="text"><h1>Calatheas are known for their beautiful foliage and tempermental personalities.</h1></span><img src="../assets/../assets/plant1.jpg"></a><p>Calathea</p></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
